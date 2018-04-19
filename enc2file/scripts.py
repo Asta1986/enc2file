4a060798765468ab78dccce1569609b9fd54d6de
Astaroth <psljp@protonmail.com>
2018-04-19 12:20:39 -0300

    added console scripts

---
+++ enc2file/scripts.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+from enc2file import Enc2File
+import click
+
+# Common strs.
+hk = 'Encryption key to use.'
+hkf = 'Path to the file with the encryption key to use.'
+he = 'Encoding to use. Default is UTF-8.'
+de = 'utf-8'
+
+
+@click.group()
+def cli():
+    pass
+
+
+@click.command()
+def gen_key():
+    """Generates an encryption key to use with enc2file commands."""
+    click.echo(Enc2File().token2str(Enc2File().get_encryption_key()))
+
+
+# Auxiliary functions to check the options set.
+def gen_enc2file(key, key_file, encoding):
+    ef = Enc2File(encoding=encoding)
+    if key is not None:
+        ef.set_encryption_key(key)
+    elif key_file is not None:
+        ef.key_from_file(key_file)
+    return ef
+
+
+def validate_key(key, key_file):
+    if key is None and key_file is None:
+        raise click.UsageError('No key specified.')
+    elif key is not None and key_file is not None:
+        raise click.UsageError('Only one key source can be used at once.')
+
+
+def validate_src(src_file, message):
+    if src_file is None and message is None:
+        raise click.UsageError('No text to decrypt given.')
+#
+
+
+@click.command()
+@click.option('--key', help=hk)
+@click.option('--key_file', help=hkf, type=click.Path())
+@click.option('--dest_file', help='Path to the file where the encrypted message will be stored.', type=click.Path())
+@click.option('--encoding', default=de, help=he)
+@click.argument('message')
+def encrypt(key, key_file, dest_file, encoding, message):
+    """Encrypts received MESSAGE using the specified key and stores it in a file or prints it to stdout."""
+    validate_key(key, key_file)
+    ef = gen_enc2file(key, key_file, encoding)
+    if dest_file is None:
+        click.echo(ef.encrypt(message))
+    else:
+        ef.enc2file(message, dest_file)
+
+
+@click.command()
+@click.option('--key', help=hk)
+@click.option('--key_file', help=hkf, type=click.Path())
+@click.option('--src_file', help='Path to the file where the encrypted message will be read from.', type=click.Path())
+@click.option('--encoding', default=de, help=he)
+@click.option('--message', help='Encrypted text to decrypt.')
+def decrypt(key, key_file, src_file, encoding, message):
+    """Decrypts MESSAGE using the specified key and prints it to stdout."""
+    ef = gen_enc2file(key, key_file, encoding)
+    validate_key(key, key_file)
+    validate_src(src_file, message)
+    if src_file is None:
+        click.echo(ef.decrypt(ef.str2token(message)))
+    else:
+        click.echo(ef.decrypt_from_file(src_file))
+
+
+cli.add_command(gen_key)
+cli.add_command(encrypt)
+cli.add_command(decrypt)
--- setup.py
+++ setup.py
@@ -2,15 +2,19 @@
 from setuptools import setup, find_packages
 
 setup(name='enc2file',
-    version='0.1',
-    description='Module to easily encrypt and decrypt strings using cryptography\'s Fernet recipe.',
-    url='https://github.com/Asta1986/enc2file',
-    author='Asta1986',
-    author_email='psljp@protonmail.com',
-    license='GPL-3',
-    packages=find_packages(),
-    install_requires=['cryptography>=2.1.4'],
-    test_suite='nose.collector',
-    tests_require=['nose>=1.3.7'],
-    include_package_data=True,
-    zip_safe=False)
+      version='0.1',
+      description='Module to easily encrypt and decrypt strings using cryptography\'s Fernet recipe.',
+      url='https://github.com/Asta1986/enc2file',
+      author='Asta1986',
+      author_email='psljp@protonmail.com',
+      license='GPL-3',
+      packages=find_packages(),
+      install_requires=['cryptography>=2.1.4', 'click>=6.7'],
+      test_suite='nose.collector',
+      tests_require=['nose>=1.3.7'],
+      entry_points='''
+                   [console_scripts]
+                   enc2file=enc2file.scripts:cli
+                 ''',
+      include_package_data=True,
+      zip_safe=False)
