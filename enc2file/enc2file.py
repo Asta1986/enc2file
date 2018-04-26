2e317ecc9aaea55c808f07624dda6ed656c4f4fd
Astaroth <psljp@protonmail.com>
2018-04-26 20:01:56 -0300

    upd comments

--- enc2file/enc2file.py
+++ enc2file/enc2file.py
@@ -27,48 +27,48 @@ class Enc2File:
     def set_encoding(self, encoding):
         self.encoding = encoding
 
-    # Returns a string encoded with encoding of the object, from the received bytes token.
     def token2str(self, token):
+        # Returns a string encoded with encoding of the object, from the received bytes token.
         return str(token, self.encoding)
 
-    # Returns a bytes token from the received string, encoded with encoding of the object.
     def str2token(self, message):
+        # Returns a bytes token from the received string, encoded with encoding of the object.
         return bytes(message.encode(self.encoding))
 
-    # Returns an encrypted str of the received str message, using the encryption key of the object.
     def encrypt(self, message):
+        # Returns an encrypted str of the received str message, using the encryption key of the object.
         return self.token2str(self.fernet.encrypt(self.str2token(message)))
 
-    # Returns the decrypted str of the received encrypted str, using the encryption key of the object.
     def decrypt(self, enc_text):
+        # Returns the decrypted str of the received encrypted str, using the encryption key of the object.
         return self.token2str(self.fernet.decrypt(self.str2token(enc_text)))
 
-    # Stores the string representation of the current key in target file.
     def key_to_file(self, target_file_path):
+        # Stores the string representation of the current key in target file.
         try:
             with open(target_file_path, 'w') as fh:
                 fh.write(self.token2str(self._key_))
         except IOError:
             print('File {} could not be accessed.'.format(target_file_path))
 
-    # Sets the current key to the one read from target file.
     def key_from_file(self, target_file_path):
+        # Sets the current key to the one read from target file.
         try:
             with open(target_file_path, 'r') as fh:
                 self.set_encryption_key(fh.read())
         except IOError:
             print('File {} could not be accessed.'.format(target_file_path))
 
-    # Writes encrypted message string to a file.
     def enc2file(self, message, target_file_path):
+        # Writes encrypted message string to a file.
         try:
             with open(target_file_path, 'w') as fh:
                 fh.write(self.encrypt(message))
         except IOError:
             print('File {} could not be accessed.'.format(target_file_path))
 
-    # Returns the decrypted string of the encrypted text read from a file.
     def decrypt_from_file(self, target_file_path):
+        # Returns the decrypted string of the encrypted text read from a file.
         try:
             with open(target_file_path, 'r') as fh:
                 return self.decrypt(fh.read())
